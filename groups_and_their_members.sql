ff1f87e6cb3d33409122c8ab2558b36528e1294e
jmguazzo <jmguazzo@gmail.com>
2016-05-12 21:43:40 -0400

    Initial

---
+++ groups_and_their_members.sql
@@ -0,0 +1,30 @@
+﻿--This query will select all groups and for each group, it will create an array with all its members
+--This query also create a member array for CREATEDB, REPLICATION and SUPERUSER
+SELECT groups.rolname AS group_name,
+    ARRAY( SELECT membre.rolname
+           FROM pg_auth_members membres
+             JOIN pg_roles group_subquery ON group_subquery.oid = membres.roleid
+             JOIN pg_roles membre ON membre.oid = membres.member
+          WHERE group_subquery.oid = groups.oid) AS member_list
+   FROM pg_roles groups
+  WHERE (EXISTS ( SELECT 1
+           FROM pg_auth_members m
+          WHERE m.roleid = groups.oid))
+UNION
+SELECT 'SUPERUSER'::name AS nom_groupe,
+    ARRAY( SELECT u.usename AS "User name"
+           FROM pg_user u
+          WHERE u.usesuper) AS liste_membres
+UNION
+SELECT 'CREATEDB'::name AS nom_groupe,
+    ARRAY( SELECT u.usename AS "User name"
+           FROM pg_user u
+          WHERE u.usecreatedb) AS liste_membres
+UNION
+SELECT 'REPLICATION'::name AS nom_groupe,
+    ARRAY( SELECT u.usename AS "User name"
+           FROM pg_user u
+          WHERE u.userepl) AS liste_membres
+  ORDER BY 1;
+
+ 
---
+++ missing_grant_on_sequence_for_serials.sql
@@ -0,0 +1,24 @@
+﻿-- This query lists all the users that have the INSERT grant on a table but don't have grant on the sequence that's 
+-- the default of a serial value in this table.
+
+select grantee
+,rtg.table_schema
+,rtg.table_name
+,(
+ select distinct ug.object_schema||'.'||ug.object_name 
+ from	information_schema.role_usage_grants ug
+ where 'nextval('''||ug.object_schema||'.'||ug.object_name||'''::regclass)' = c.column_default
+) as "sequence"
+
+from information_schema.role_table_grants rtg
+inner join information_schema.columns c on rtg.table_schema=c.table_schema and rtg.table_name=c.table_name
+where privilege_type='INSERT'
+and column_default like 'nextval%'
+
+and grantee not in (
+ select grantee
+ from	information_schema.role_usage_grants ug
+ where 'nextval('''||ug.object_schema||'.'||ug.object_name||'''::regclass)' = c.column_default
+)
+and grantee not in (SELECT u.usename FROM pg_user u WHERE u.usesuper)
+limit 100
